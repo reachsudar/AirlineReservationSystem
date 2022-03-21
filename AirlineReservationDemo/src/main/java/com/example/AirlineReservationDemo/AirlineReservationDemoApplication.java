b7cf905dd67901e8558e5ec904ddc67df65860fd
reachsudar <51194939+reachsudar@users.noreply.github.com>
2022-03-22 10:21:26 +1100

    initial commit

---
+++ AirlineReservationDemo/src/main/java/com/example/AirlineReservationDemo/AirlineReservationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.AirlineReservationDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineReservationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlineReservationDemoApplication.class, args);
+	}
+
+}
